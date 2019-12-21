35575b82c6fce9fb30531ac87d1dce70d668838a
Deon Tan <deonjtan@gmail.com>
2019-12-21 21:37:21 +0000

    day3 part 2 in clojure

--- day3/clj/src/day3.clj
+++ day3/clj/src/day3.clj
@@ -29,23 +29,44 @@
   (repeat distance (direction u-d-l-r)))
 
 (defn trace [origin wire]
-  (reductions add origin (mapcat steps wire)))
+  (->> wire
+       (mapcat steps)
+       (reductions add origin)
+       (into [])))
 
 (defn manhattan-distance [[x1 y1] [x2 y2]]
   (+ (Math/abs (- x2 x1))
      (Math/abs (- y2 y1))))
 
+(defn find-best [results]
+  (->> results
+       (remove #(-> % first (= [0 0])))
+       (sort-by second)
+       first
+       second))
+
+(defn find-point [point points]
+  (let [step-counts (partial map-indexed vector)]
+    (->> points
+         step-counts
+         (filter (fn [[steps p]]
+                   (= point p)))
+         first)))
+
+(defn get-steps-to-intersect [w1-points w2-points point]
+  (let [w1-steps (first (find-point point w1-points))
+        w2-steps (first (find-point point w2-points))]
+    (+ w1-steps w2-steps)))
+
 (defn -main []
-  (let [[w1 w2] (parse-input "/tmp/aoc3_test")
+  (let [[w1 w2] (parse-input "/tmp/aoc3")
         origin [0 0]
         w1-points (trace origin w1)
-        w2-points (trace origin w2)]
-    (->> [w1-points w2-points]
-         (map set)
-         (reduce set/intersection)
+        w2-points (trace origin w2)
+        intersections (->> [w1-points w2-points]
+                           (map set)
+                           (reduce set/intersection))]
+    (->> intersections
          (map (juxt identity
-                    (partial manhattan-distance origin)))
-         (remove #(-> % first (= [0 0])))
-         (sort-by second)
-         first
-         second)))
+                    (partial get-steps-to-intersect w1-points w2-points)))
+         find-best)))
