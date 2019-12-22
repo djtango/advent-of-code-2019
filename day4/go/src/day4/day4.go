034330e21c98e9105ecfb7ae537450345e2c4ae1
Deon Tan <deonjtan@gmail.com>
2019-12-22 21:01:35 +0000

    day4 part 2 go

--- day4/go/src/day4/day4.go
+++ day4/go/src/day4/day4.go
@@ -12,19 +12,34 @@ const upperBound string = "654504"
 func inspect(s string) bool {
 	l := len(s)
 	i, j := 0, 1
-	hasAdjacentDigits := false
 	allNotDecreasing := true
+	hasAdjacentValue := false
+	counts := map[string]int{
+		"0": 0,
+		"1": 0,
+		"2": 0,
+		"3": 0,
+		"4": 0,
+		"5": 0,
+		"6": 0,
+		"7": 0,
+		"8": 0,
+		"9": 0,
+	}
 	for j < l {
-		hasAdjacentDigits = hasAdjacentDigits || s[i] == s[j]
+		counts[string(s[i])] += 1
 		allNotDecreasing = allNotDecreasing && s[i] <= s[j]
 		i += 1
 		j += 1
 	}
-	return hasAdjacentDigits && allNotDecreasing
+	counts[string(s[i])] += 1
+	for _, v := range counts {
+		hasAdjacentValue = hasAdjacentValue || v == 2
+	}
+	return allNotDecreasing && hasAdjacentValue
 }
 
-func main() {
-	fmt.Println("--------------------")
+func checkRange(lower, upper int) int {
 	i := 138307
 	end := 654504
 	count := 0
@@ -34,6 +49,11 @@ func main() {
 		}
 		i += 1
 	}
-	fmt.Println(count)
+	return count
+}
+
+func main() {
+	fmt.Println("--------------------")
+	fmt.Println(checkRange(138307, 654504))
 	fmt.Println("--------------------")
 }
